1ac38acdae2c8ba41e5c48e40b90c3721c7b32f2
anastasia1sl <slobodyanik.a97@gmail.com>
2025-08-06 22:41:47 +0300

    fonts, basic scss added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.scss";
+import icons from "./img/icons/icons.svg";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h2>hello, world</h2>
+      <svg width={26} height={26}>
+        <use href={`${icons}#icon-x-button`} />
+      </svg>
+    </>
+  );
 }
 
 export default App;
