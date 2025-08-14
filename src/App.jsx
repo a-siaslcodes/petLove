87c5f4b2bfe88c79712177a4e9a8881f8569d509
anastasia1sl <slobodyanik.a97@gmail.com>
2025-08-14 16:15:51 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <LoginPage />
-    </>
+      <Routes>
+        <Route path="/" element />
+        <Route path="/news" element />
+        {/* <Route path="/notices" element /> find pet page  */}
+        <Route path="/friends" element />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/register" element />
+      </Routes>
+    </div>
   );
 }
 
